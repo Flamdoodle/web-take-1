98f27cc8675729b393de9396265315d795e17125
wesuite <wesuite@gabrys-MacBook-Pro.local>
2016-06-20 15:29:21 -0400

    wp

--- src/app/left-nav/left-nav.component.spec.ts
+++ src/app/left-nav/left-nav.component.spec.ts
@@ -17,11 +17,9 @@ import { NavigationService} from '../navigation.service'
 
 describe('Component: LeftNav', () => {
   it('should create an instance', () => {
-<<<<<<< HEAD
+
     let service = new NavigationService();
-=======
-    let service = new NavigationService;
->>>>>>> 17c220a684bb8a3ca6bd232472dd1fd24c2c3e7c
+
     let component = new LeftNavComponent(service);
     expect(component).toBeTruthy();
   });
