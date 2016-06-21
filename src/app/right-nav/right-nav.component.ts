40fc870fb43023602bbd93b42dc0f4947681399b
melegant <melegant@gmail.com>
2016-06-21 10:56:11 -0400

    testing stuff out

---
+++ src/app/right-nav/right-nav.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+selector: 'app-right-nav',
+template: '<p> test </p>'
+//templateUrl: 'right-nav.component.html'
+})
+
+export class RightNavComponent
+{}
+
+
