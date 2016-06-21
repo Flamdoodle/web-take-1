e6138b9d9b4725c0b792ef5a398ce796bcf18a55
wesuite <wesuite@gabrys-MacBook-Pro.local>
2016-06-21 13:42:47 -0400

    add working e2e tests

--- e2e/app.e2e.ts
+++ e2e/app.e2e.ts
@@ -7,8 +7,30 @@ describe('webstimate App', function() {
     page = new WebstimatePage();
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('webstimate works!');
+//   it('should display message saying app works', () => {
+//     page.navigateTo();
+//     expect(page.getParagraphText()).toEqual('webstimate works!');
+//   });
+
+  describe('webstimate menu burger', function() {
+
+    it('click one on the menu and hide labels', () => {
+      page.navigateTo();
+      var elToClick=element(by.css('.glyphicon-menu-hamburger'))
+      elToClick.click();
+      var burgerDiv=element(by.id('burgerDiv'));
+
+      expect(burgerDiv.getAttribute('class')).toEqual('borderHidden');
+
+    });
+
+    it('expands the menu after clicking again', () => {
+      var elToClick=element(by.css('.glyphicon-menu-hamburger'))
+      elToClick.click();
+      var burgerDiv=element(by.id('burgerDiv'));
+
+      expect(burgerDiv.getAttribute('class')).toEqual('border');
+    });
   });
+
 });
