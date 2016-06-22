ef4205bb91e664726520524ac49981255fe4ddf2
Stephen Roth <stephenroth.sr@gmail.com>
2016-06-22 15:43:21 -0400

    added the new prospect form

--- src/app/webstimate.component.ts
+++ src/app/webstimate.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { LeftNavComponent } from './left-nav/left-nav.component';
-import { RightNavComponent } from './right-nav/right-nav.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   moduleId: module.id,
   selector: 'webstimate-app',
   templateUrl: 'webstimate.component.html',
   styleUrls: ['webstimate.component.css'],
-  directives: [LeftNavComponent, RightNavComponent]
+  directives: [LeftNavComponent, ContentComponent]
 })
 export class WebstimateAppComponent {
   title = 'webstimate works!';
